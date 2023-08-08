0a62195bea355bbbb594c9879d812ae4dec657b3
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-08-08 19:19:36 -0300

    feat/add some components folders, add button mixin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,11 @@ import styles from "./styles.module.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    const classes = `styles.button styles.purpleButton`;
 
     return (
         <main>
             <Button className={styles.purpleButton} text="Text Button" />
-            <Button className={styles.blueButton} text="Text Button" />
+            <Button className={styles.pinkButton} text="Text Button" />
             <h1>HELLO WORLD</h1>
         </main>
     );
