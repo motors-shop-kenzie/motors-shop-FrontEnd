f95b5d3e560fdd39807614fc8a6cf0d3963a8b38
Richard <richardpires663802@gmail.com>
2023-08-09 12:51:35 -0300

    feat:settings footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import { IoIosArrowUp } from "react-icons/io";
+export const Footer = () => {
+  return (
+    <footer>
+      <h2>
+        MOTORS <span>shop</span>
+      </h2>
+
+      <p>© 2022 - Todos os direitos reservados.</p>
+
+      <button>
+        <IoIosArrowUp />
+      </button>
+    </footer>
+  );
+};
