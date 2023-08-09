58e516cd688838a76b82f666023ab2098dd60165
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2023-08-09 18:50:21 -0300

    feat/inputs, labels and textarea created

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,10 @@
+const Select = () => {
+    return (
+        <select name="Selecione um opção" id="">
+            <option value="">Opção 1</option>
+            <option value="">Opção 2</option>
+        </select>
+    );
+};
+
+export { Select };
