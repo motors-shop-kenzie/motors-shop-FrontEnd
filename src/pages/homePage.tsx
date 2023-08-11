c3665d30a3cd73d79d11f5c3e2ee1dfee1105c01
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-08-11 13:04:08 -0300

    refactor/homePage

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,18 +1,13 @@
 import CarFilter from "@/components/CarFilter";
-import { LoginForm } from "@/components/Forms/Login";
-import { Modal } from "@/components/Modal";
-import { ModalCreateCar } from "@/components/Modal/Modal";
-import { ModalContext } from "@/contexts/Modal";
+import { Footer } from "@/components/Footer";
+import { NavBar } from "@/components/NavBar";
+import { ProductBox } from "@/components/ProductBox";
+import { ProductCard } from "@/components/ProductCard";
 import { TCarProduct } from "@/interfaces/CarProduc";
 import api from "@/services/api";
 import { GetServerSideProps } from "next";
-import { useContext } from "react";
-import Home from "./testes";
-import { ProductBox } from "@/components/ProductBox";
-import { ProductCard } from "@/components/ProductCard";
-import { Footer } from "@/components/Footer";
-import { NavBar } from "@/components/NavBar";
 import styles from "./styles.module.scss";
+
 
 interface HomePageProps {
   cars: TCarProduct[];
@@ -22,26 +17,25 @@ export default function HomePage({ cars }: HomePageProps) {
   return (
     <main className={styles.body}>
       <NavBar dealer logged />
-      <section>
-        <h1>Titulo ficticio</h1>
-        {/* {showModal === "createContact" && <ModalCreateCar />} */}
+      <div className={styles.bg_img}></div>
+      <section className={styles.car_shop}>
         <CarFilter
-          // aqui devo passar a lista de carros que vão vir através da api
           cars={[]}
           onFilterChange={(filters) => console.log(filters)}
-        />{" "}
+        />
+        {""}
+        <ProductBox>
+          {cars.map((car) => {
+            return <ProductCard key={car.id} car={car} />;
+          })}
+        </ProductBox>
       </section>
-      <ProductBox>
-        {cars.map((car) => {
-          return <ProductCard key={car.id} car={car} />;
-        })}
-      </ProductBox>
       <Footer />
     </main>
   );
 }
 
-const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const response = await api.get<TCarProduct[]>("/cars");
   return {
     props: {
