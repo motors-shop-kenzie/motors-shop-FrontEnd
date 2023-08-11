fe2b7119c0f8615fdfea46a1f9394e4664ec38b2
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-08-11 18:21:58 -0300

    create a filter logic

--- src/components/CarFilter/index.tsx
+++ src/components/CarFilter/index.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useState } from "react";
-import { CarFilterProps, FilterOptions } from "./interface";
+import { Car, CarFilterProps, FilterOptions } from "./interface";
 
 export default function CarFilter({ cars, onFilterChange }: CarFilterProps) {
   const [selectedFilters, setSelectedFilters] = useState<FilterOptions>({
@@ -16,111 +16,76 @@ export default function CarFilter({ cars, onFilterChange }: CarFilterProps) {
   ) => {
     const newFilters = { ...selectedFilters, [filterType]: value };
     setSelectedFilters(newFilters);
-    onFilterChange(newFilters);
+
+    applyFilters(newFilters);
   };
+
+  const applyFilters = (filters: FilterOptions) => {
+    const filteredCars = cars.filter((car) =>
+      Object.entries(filters).every(
+        ([filterType, filterValue]) =>
+          filterValue === null || car[filterType as keyof Car] === filterValue
+      )
+    );
+
+    onFilterChange(
+      Object.values(filters).some((value) => value !== null)
+        ? filteredCars
+        : cars
+    );
+  };
+
+  const renderSelect = (filterType: keyof FilterOptions, options: string[]) => (
+    <div key={filterType}>
+      <h3>{filterType}</h3>
+      <select
+        value={selectedFilters[filterType] || ""}
+        onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+          handleFilterChange(
+            filterType,
+            e.target.value !== "" ? e.target.value : null
+          )
+        }
+      >
+        <option value="">Todos</option>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+
+  const brandOptions = [
+    "General Motors",
+    "Fiat",
+    "Ford",
+    "Honda",
+    "Porsche",
+    "Volswagen",
+  ];
+  const modelOptions = [
+    "Civic",
+    "Corolla",
+    "Cruze",
+    "Fit",
+    "Gol",
+    "Ka",
+    "Onix",
+    "Porsche 718",
+  ];
+  const colorOptions = ["Azul", "Branca", "Cinza", "Prata", "Preta", "Verde"];
+  const yearOptions = [2022, 2021, 2018, 2015, 2013, 2012, 2010];
+  const gasolineOptions = ["Elétrico", "Flex", "Híbrido"];
 
   return (
     <div>
-      <div>
-        <h3>Marca</h3>
-        <select
-          value={selectedFilters.brand || ""}
-          onChange={(e: ChangeEvent<HTMLSelectElement>) =>
-            handleFilterChange(
-              "brand",
-              e.target.value !== "" ? e.target.value : null
-            )
-          }
-        >
-          <option value="">Todos</option>
-          <option value="General Motors">General Motors</option>
-          <option value="Fiat">Fiat</option>
-          <option value="Ford">Ford</option>
-          <option value="Honda">Honda</option>
-          <option value="Porsche">Porsche</option>
-          <option value="Volswagen">Volswagen</option>
-        </select>
-      </div>
-      <div>
-        <h3>Modelo</h3>
-        <select
-          value={selectedFilters.model || ""}
-          onChange={(e: ChangeEvent<HTMLSelectElement>) =>
-            handleFilterChange(
-              "model",
-              e.target.value !== "" ? e.target.value : null
-            )
-          }
-        >
-          <option value="">Todos</option>
-          <option value="Civic">Civic</option>
-          <option value="Corolla">Corolla</option>
-          <option value="Cruze">Cruze</option>
-          <option value="Fit">Fit</option>
-          <option value="Gol">Gol</option>
-          <option value="Ka">Ka</option>
-          <option value="Onix">Onix</option>
-          <option value="Porsche 718">Porsche 718</option>
-        </select>
-      </div>
-      <div>
-        <h3>Cores</h3>
-        <select
-          value={selectedFilters.color || ""}
-          onChange={(e: ChangeEvent<HTMLSelectElement>) =>
-            handleFilterChange(
-              "color",
-              e.target.value !== "" ? e.target.value : null
-            )
-          }
-        >
-          <option value="">Todas</option>
-          <option value="Azul">Azul</option>
-          <option value="Branca">Branca</option>
-          <option value="Cinza">Cinza</option>
-          <option value="Prata">Prata</option>
-          <option value="Preta">Preta</option>
-          <option value="Verde">Verde</option>
-        </select>
-      </div>
-      <div>
-        <h3>Ano</h3>
-        <select
-          value={selectedFilters.year || ""}
-          onChange={(e: ChangeEvent<HTMLSelectElement>) =>
-            handleFilterChange(
-              "year",
-              e.target.value !== "" ? parseInt(e.target.value) : null
-            )
-          }
-        >
-          <option value="">Todos</option>
-          <option value={2022}>2022</option>
-          <option value={2021}>2021</option>
-          <option value={2018}>2018</option>
-          <option value={2015}>2015</option>
-          <option value={2013}>2013</option>
-          <option value={2012}>2012</option>
-          <option value={2010}>2010</option>
-        </select>
-      </div>
-      <div>
-        <h3>Combustível</h3>
-        <select
-          value={selectedFilters.gasoline || ""}
-          onChange={(e: ChangeEvent<HTMLSelectElement>) =>
-            handleFilterChange(
-              "gasoline",
-              e.target.value !== "" ? e.target.value : null
-            )
-          }
-        >
-          <option value="">Todos</option>
-          <option value="Elétrico">Elétrico</option>
-          <option value="Flex">Flex</option>
-          <option value="Híbrido">Híbrido</option>
-        </select>
-      </div>
+      {renderSelect("brand", brandOptions)}
+      {renderSelect("model", modelOptions)}
+      {renderSelect("color", colorOptions)}
+      {renderSelect("year", yearOptions.map(String))}
+      {renderSelect("gasoline", gasolineOptions)}
     </div>
   );
 }
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -3,43 +3,40 @@ import { Footer } from "@/components/Footer";
 import { NavBar } from "@/components/NavBar";
 import { ProductBox } from "@/components/ProductBox";
 import { ProductCard } from "@/components/ProductCard";
-import { TCarProduct } from "@/interfaces/CarProduc";
-import api from "@/services/api";
-import { GetServerSideProps } from "next";
 import styles from "./styles.module.scss";
+import { useContext, useEffect, useState } from "react";
+import { CarsContext } from "@/contexts/Cars/CarsContext";
+import { Car } from "@/components/CarFilter/interface";
 
+export default function HomePage() {
+  const { getAllCarsRequest, cars } = useContext(CarsContext);
+  const [filteredCars, setFilteredCars] = useState<Car[]>([]);
 
-interface HomePageProps {
-  cars: TCarProduct[];
-}
+  useEffect(() => {
+    getAllCarsRequest();
+  }, []);
 
-export default function HomePage({ cars }: HomePageProps) {
+  const handleFilterChange = (filteredCars: Car[]) => {
+    setFilteredCars(filteredCars);
+  };
+
+  console.log(cars);
+
   return (
     <main className={styles.body}>
       <NavBar dealer logged />
       <div className={styles.bg_img}></div>
       <section className={styles.car_shop}>
-        <CarFilter
-          cars={[]}
-          onFilterChange={(filters) => console.log(filters)}
-        />
-        {""}
+        <CarFilter cars={filteredCars} onFilterChange={handleFilterChange} />
         <ProductBox>
-          {cars.map((car) => {
-            return <ProductCard key={car.id} car={car} />;
-          })}
+          {filteredCars.length === 0 ? (
+            <p>Não há carros correspondentes aos filtros.</p>
+          ) : (
+            filteredCars.map((car) => <ProductCard key={car.id} car={car} />)
+          )}
         </ProductBox>
       </section>
       <Footer />
     </main>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await api.get<TCarProduct[]>("/cars");
-  return {
-    props: {
-      cars: response.data,
-    },
-  };
-};
