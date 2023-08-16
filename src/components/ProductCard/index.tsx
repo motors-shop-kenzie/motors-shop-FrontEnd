8d12d373b8f7eeb2b68f0c105d27afbec440333b
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-08-16 18:50:47 -0300

    refactor/page>index

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -7,18 +7,10 @@ import { TCarProduct } from "@/interfaces/CarProduc";
 
 interface CardProps {
   car: TCarProduct;
-<<<<<<< HEAD
-  user: TUser;
-}
-
-export const ProductCard = ({ car, user }: CardProps) => {
-  // console.log(user.name[0])
-=======
   user: TUser | undefined;
 }
 
 export const ProductCard = ({ car, user }: CardProps) => {
->>>>>>> 18a6fa3e494b53cbf558e265fc3dae30bec5f71b
   return (
     <div className={styles.card}>
       <div className={styles.img}>
@@ -31,11 +23,7 @@ export const ProductCard = ({ car, user }: CardProps) => {
         <p className={styles.title}>{car.name}</p>
       </div>
       <div className={styles.desc}>{car.description}</div>
-<<<<<<< HEAD
-      <UserHeader user={user.name} index={user.id} letter={user.name[0]} />
-=======
       <UserHeader user={user} index={user?.id} letter={user?.name?.charAt(0)} />
->>>>>>> 18a6fa3e494b53cbf558e265fc3dae30bec5f71b
       <div className={styles.carInfo}>
         <div className={styles.info}>
           <div>{car.km} KM</div> <div>{car.year}</div>
