01d9465a197dd735e239f46eb3ed6c88869e1dbc
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2023-08-16 18:06:36 -0300

    feat: login and register styles

--- src/components/Forms/Login/index.tsx
+++ src/components/Forms/Login/index.tsx
@@ -7,43 +7,54 @@ import { Button } from "@/components/Button";
 import InputStyles from "../../Input/styles.module.scss";
 import ButtonStyles from "../../Button/styles.module.scss";
 import Link from "next/link";
+import { InputSectionField } from "@/components/InputSectionField";
 
 export const LoginForm = () => {
   return (
     <div className={styles.loginContainer}>
       <form>
-        <h1>Login</h1>
+        <div>
+          <h1>Login</h1>
 
-        <Label htmlFor="email" name="Email" />
-        <InputFocus>
-          <Input
-            type="email"
-            className={InputStyles.basicInputWithBorder}
-            placeholder="Digitar email"
-            id="email"
-          />
-        </InputFocus>
+          <div className={styles.inputsSection}>
+            <InputSectionField>
+              <Label htmlFor="email" name="Email" />
+              <InputFocus>
+                <Input
+                  type="email"
+                  className={InputStyles.basicInputWithBorder}
+                  placeholder="Digitar email"
+                  id="email"
+                />
+              </InputFocus>
+            </InputSectionField>
 
-        <Label htmlFor="password" name="Senha" />
-        <InputFocus>
-          <Input
-            type="password"
-            className={InputStyles.basicInputWithBorder}
-            placeholder="Digitar senha"
-            id="password"
-          />
-        </InputFocus>
+            <InputSectionField>
+              <Label htmlFor="password" name="Senha" />
+              <InputFocus>
+                <Input
+                  type="password"
+                  className={InputStyles.basicInputWithBorder}
+                  placeholder="Digitar senha"
+                  id="password"
+                />
+              </InputFocus>
+            </InputSectionField>
+          </div>
 
-        <p>Esqueci minha senha</p>
+          <p>Esqueci minha senha</p>
 
-        <Button className={ButtonStyles.brand1Button} text="Entrar" />
+          <div className={styles.buttonsSection}>
+            <Button className={ButtonStyles.brand1Button} text="Entrar" />
 
-        <p>Ainda não possui conta?</p>
+            <p className={styles.textBetweenButtons}>Ainda não possui conta?</p>
 
-        <Button
-          className={ButtonStyles.grey10BorderGrey4TextGrey0Button}
-          text="Cadastrar"
-        />
+            <Button
+              className={ButtonStyles.grey10BorderGrey4TextGrey0Button}
+              text="Cadastrar"
+            />
+          </div>
+        </div>
       </form>
     </div>
   );
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import styles from "./styles.module.scss";
+
 import { Footer } from "@/components/Footer";
 import { LoginForm } from "@/components/Forms/Login";
 import { NavBar } from "@/components/NavBar";
 
 export default function Register() {
   return (
-    <main>
+    <>
       <NavBar dealer /* className={styles.cover} */ />
-      <LoginForm></LoginForm>
+      <main className={styles.containerLoginPage}>
+        <LoginForm></LoginForm>
+      </main>
       <Footer path="/register" />
-    </main>
+    </>
   );
 }
