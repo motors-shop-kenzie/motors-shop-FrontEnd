b322c81155f36099d0f7d17c1635a8e1fc8a0fb3
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2023-08-16 18:54:31 -0300

    fix:styles of inputfield

--- src/components/InputSectionField/index.tsx
+++ src/components/InputSectionField/index.tsx
@@ -4,8 +4,3 @@ import styles from "./styles.module.scss";
 export const InputSectionField = ({ children }: iChildrenProps) => {
   return <section className={styles.inputSection}>{children}</section>;
 };
-<<<<<<< HEAD
-=======
-
- 
->>>>>>> 18a6fa3e494b53cbf558e265fc3dae30bec5f71b
