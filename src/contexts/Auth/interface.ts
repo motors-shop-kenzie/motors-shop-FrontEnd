f0b8d0f47be96101e56a7beec73e365717d646f6
RafaelFreitas-cloud <raf.mec.ba@gmail.com>
2023-08-22 17:58:19 -0300

    refactor/modal close

--- src/contexts/Auth/interface.ts
+++ src/contexts/Auth/interface.ts
@@ -16,4 +16,8 @@ export interface IAuthContext {
   loggedUser: () => Promise<void>;
   sendEmail: (sendEmailResetPasswordData: SendEmailResetPasswordData) => void;
   resetPassword: (resetPasswordData: ResetPasswordData, token: string) => void;
+  openNavBar: boolean;
+  setOpenNavBar: Dispatch<SetStateAction<boolean>>;
+  toggleNavBar: () => void;
+  closeNavBar: () => void;
 }
