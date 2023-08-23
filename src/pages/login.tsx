dd83608ccbdbe2d688a1c956821aebd1ded7e8db
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-08-23 18:11:02 -0300

    fix: register/login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,26 +5,15 @@ import { LoginForm } from "@/components/Forms/Login";
 import { NavBar } from "@/components/NavBar";
 import styles from "./styles.module.scss";
 import PageLoading from "@/components/PageLoading";
-import { useContext, useEffect } from "react";
-import { LoadingContext } from "@/contexts/Loading";
 
 export default function Login() {
-  const { loading } = useContext(LoadingContext);
   return (
-    <>
-      {loading ? (
-        <div className={styles.example}>
-          <h1>Carregando...</h1>
-        </div>
-      ) : (
-        <>
-          <main className={styles.containerLoginPage}>
-            <LoginForm />
-          </main>
-          <Footer path="/register" />
-          <NavBar />
-        </>
-      )}
-    </>
+    <PageLoading>
+      <main className={styles.containerLoginPage}>
+        <LoginForm />
+      </main>
+      <Footer path="/register" />
+      <NavBar />
+    </PageLoading>
   );
 }
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -5,26 +5,15 @@ import { RegisterForm } from "@/components/Forms/Register";
 import { NavBar } from "@/components/NavBar";
 import styles from "./styles.module.scss";
 import PageLoading from "@/components/PageLoading";
-import { useContext } from "react";
-import { LoadingContext } from "@/contexts/Loading";
 
 export default function Register() {
-  const { loading } = useContext(LoadingContext);
   return (
-    <>
-      {loading ? (
-        <div>
-          <h1>Carregando...</h1>
-        </div>
-      ) : (
-        <>
-          <main className={styles.containerRegisterPage}>
-            <RegisterForm />
-          </main>
-          <Footer path="/register" />
-          <NavBar />
-        </>
-      )}
-    </>
+    <PageLoading>
+      <main className={styles.containerRegisterPage}>
+        <RegisterForm />
+      </main>
+      <Footer path="/register" />
+      <NavBar />
+    </PageLoading>
   );
 }
