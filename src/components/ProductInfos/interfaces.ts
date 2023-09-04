186d8f2a2d01e36ace8878c029809e4bf752f408
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-09-04 09:28:57 -0300

    fix: fix type declaration in whatsapp function

--- src/components/ProductInfos/interfaces.ts
+++ src/components/ProductInfos/interfaces.ts
@@ -1,4 +1,4 @@
-import { Car } from "@/interfaces/CarFilter";
+import { TCarProduct } from "@/interfaces/CarProduc";
 
 export interface IVehicleInfo {
   brand: string;
@@ -8,7 +8,7 @@ export interface IVehicleInfo {
 }
 
 export interface IOpenWhatsApp {
-  singleCar: Car | undefined;
+  singleCar: TCarProduct | undefined;
   message: string;
 }
 
